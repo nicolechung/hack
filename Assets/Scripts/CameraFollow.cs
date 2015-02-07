d42a05938ff8a708121e287be2bf81c10ff32d88
Nicole Chung <nicole.chung@gmail.com>
2015-02-07 12:00:04 -0500

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("camera script working");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# gay-bathhouse-hack
