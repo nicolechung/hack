679bba080f6ab97dcbdabda223e26505f2ffe82a
Nicole Chung <nicole.chung@gmail.com>
2015-02-07 12:49:10 -0500

    added Camera Follow script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("camera script working");
@@ -10,6 +14,13 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
 	}
 }
