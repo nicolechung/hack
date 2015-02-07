be0b0aa6473595be4e9219404dad524bd989c017
Nicole Chung <nicole.chung@gmail.com>
2015-02-07 13:03:13 -0500

    starting Spawning scripts

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public float spawnTime = 5f;
+	public float spawnDelay = 3f;
+	public GameObject[] towelMen; /* guys on the prowl... */
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("starting spawn");
+		// start calling the spawn function repeatedly after delay
+		InvokeRepeating ("Spawn", spawnDelay, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// instantiate a random enemy
+		int towelMenIndex = Random.Range (0, towelMen.Length);
+		Instantiate (towelMen[towelMenIndex], transform.position, transform.rotation);
+	}
+}
---
+++ Assets/Scripts/TowelMan.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowelMan : MonoBehaviour {
+
+	public float speed;
+
+	// states
+	private bool isChasing;
+	private bool isLoving;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Love() {
+		// go to shower zone
+		Debug.Log ("loving behaviour");
+	}
+
+	void Chase() {
+		// seek 
+		Debug.Log ("seek behaviour");
+	}
+}
